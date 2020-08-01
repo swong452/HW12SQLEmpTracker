b1a405c68568cd627d95b9e8b97a933294be1b8b
samson wong <steelerwong2005@hotmail.com>
2020-08-01 01:50:24 -0400

    Add employee (without manager), add role, view employee

--- empTracker.js
+++ empTracker.js
@@ -1,41 +1,43 @@
 var inquirer = require("inquirer");
-//const sqlDB = require("./sqlTask");
-//const db = require("../db");
+const { prompt } = require("inquirer");
 const db = require("./sqlTask");
 require("console.table");
 
 startMenu();
 
-function startMenu() {
+async function startMenu() {
     inquirer.prompt(
         {
             type: "list",
             name: "selection",
             choices: ["Add Employee", "Remove Employee", "Update Employee",
                 "View Employee", "Add Department", "Remove Department", "View All Department",
-                "Add Role", "Remove Role", "View All Roles"],
+                "Add Role", "Remove Role", "View All Roles", "Quit"],
             message: "Please choose one option"
         }).then(function (choice) {
 
             switch (choice.selection) {
                 case "Add Employee":
-                //addDept();  // Start to add dept, then will call addRole() and addEmp()
+                    return empInfo();  // Start to add dept, then will call addRole() and addEmp()
                 case "Remove Employee":
-                    removeEmp();
-                    break;
+                    return removeEmp();
                 case "Update Employee":
-                    updateEmp();
+                    return updateEmp();
                 case "View Employee":
-                    viewEmpScreen(); // View by Emp Manager
+                    return viewEmpScreen(); // View by Emp Manager
+                case "Add Role":
+                    return addNewRole(); // View by Emp Manager
+                case "Add Department":
+                    return addNewDept(); // View by Emp Manager
                 default:
-                //console.log("default");
+                    return quit();
             }
         })
 }
 
 // This function will collect new employee data. ANd try to Department first
-function addDept() {
-    console.log("Add employee Function");
+async function empInfo() {
+    console.log("Collect New Employee Info");
     // Collect new employee info
     inquirer.prompt([
 
@@ -45,76 +47,118 @@ function addDept() {
         { type: "input", name: "title", message: "What is new employee Title ?" },
         { type: "input", name: "salary", message: "What is new employee Salary ?" },
         { type: "input", name: "manager", message: "What is new employee Manager (if no, hit enter) ?" }
-    ]).then(function (empData) {
+    ]).then(async function (empData) {
+
         //Add Dept if not exists
-        var deptExist = "select name from department where name = ?"
-        connection.query(deptExist, [empData.dept], function (err, response) {
-            console.log("Returned Res is:", response);
-            //console.log("Returned Res NAME is:", response[0].name);
+        var dept = await db.ifDeptExist(empData.dept);
+        console.log("dept returned", dept);
+        if (dept.length > 0) {
+            // As Dept already exists. No need to add new dept. Go to check role
+            console.log("This dept already exists");
 
-            // when select statement return sth, response is NOT an empty array [], 
-            // meaning this is a new department
-            if (response.length > 0) {
-                // As Dept already exists. No need to add new dept. Go to check role
-                console.log("This dept already exists");
-                addRole(empData)
-            } else {
-                var insertSql = "insert into department set name = ?"
-                connection.query(insertSql, [empData.dept], function (err, res) {
-                    if (err) throw err;
-                    console.log("Added Dept: ", res);
-                    addRole(empData, res.insertId)
-                });
-            }
-        })
-    })
-}
-
-// Add unique Title to the Role table.
-function addRole(empData, deptID) {
-    console.log("Add Role with these 2 x param:", empData, deptID);
-    //Check if Title already exists
-    var roleExist = "select title from roles where title = ?"
-    connection.query(roleExist, [empData.title], function (err, response) {
-        console.log("Returned Role Res is:", response);
-
-        // when select statement return sth, response array len > 0 
-        // meaning this department already exist.
-        if (response.length > 0) {
-            // As Role already exists. No need to add new dept. Go to check role
-            console.log("This Role alredy exists");
-            // Need to extract this Role ID, and pass to addEmp()
-            addEmp(empData, "3")
+            // Get the current DeptID; pass it to create new Role if needed
+            var deptID = await db.getDeptID(empData.dept);
+            addRole(empData, deptID[0].id, true)
         } else {
-            var insertSql = "insert into roles set title = ?, salary = ?, department_id = ?"
-            connection.query(insertSql, [empData.title, empData.salary, deptID], function (err, res) {
-                if (err) throw err;
-                console.log("Added Role: ", res);
-                // if empData.manager != NULL, select the mgr id from employee table
-                // then pass this id to addEmp()
-
-                addEmp(empData, res.insertId) // Pass that role ID, to employee tbl to fill role_id field 
-            });
+            // New department
+            var newDept = await db.addDept(empData.dept);
+            addRole(empData, newDept.insertId, true);
         }
-    })
+
+    }) // end .then
+} // end addDept
+
+// Add a new role ONLY - not necessary add new emp
+async function addNewRole() {
+    console.log("Add Brand New role");
+
+    // Create a Department List array with all fields from mysql
+    const departments = await db.listAllDept();
+
+    // convert previously created department object, into new array that
+    // only has 2 x kv pair. Such that, this new arry used in inquirer prompt
+    const deptChoice = departments.map(({ id, name }) => ({
+        name: name,
+        value: id
+    }));
+
+    console.log("new deptChoice", deptChoice);
+
+    // Create an object roleData that has these fields: title, salary, and department
+    const roleData = await prompt([
+        {
+            name: "title",
+            message: "What is the name of the Newrole?"
+        },
+        {
+            name: "salary",
+            message: "What is the salary of the role?"
+        },
+        {
+            type: "list",
+            name: "dept_id",
+            message: "Which department does the role belong to?",
+            choices: deptChoice
+        }
+    ]);
+    console.log("New roleData obj: ", roleData);
+    addRole(roleData, roleData.dept_id, false);
+
 }
 
+// Add existing OR New Title to the Role table from New Employee
+async function addRole(empData, deptID, newEmp = true) {
+    console.log("From new emp - check Role with these 2 x param:", empData, deptID);
+    //Check if Title already exists
 
-function addEmp(empData, roleID) {
+    var role = await db.ifRoleExist(empData.title);
+    // when select statement return sth, response array len > 0 
+    // meaning this Role already exist.
+    if (role.length > 0) {
+        // As Role already exists. Extract this Role ID, and pass to addEmp()
+        var roleID = await db.getRoleID(empData.title);
+        console.log("This Role already exists with Role ID", roleID, roleID[0].id);
+        if (newEmp == true) {
+            addEmp(empData, roleID[0].id);
+        } else {
+            // For add new role, after checking, just get back to menu
+            startMenu();
+        }
+    } else {
+        // Create new role using the passed in department iD
+        var newRole = await db.addRole(empData.title, empData.salary, deptID);
+        // if empData.manager != NULL, select the mgr id from employee table
+        // then pass this id to addEmp()
+        if (newEmp == true) {
+            addEmp(empData, newRole.insertId) // Pass that role ID, to employee tbl to fill role_id field 
+        } else {
+            // For add new role, after checking, just get back to menu
+            startMenu();
+        }
+    };
+} // end addRole
+
+
+// Add new employee
+async function addEmp(empData, roleID) {
     console.log("Add emp with these 2 x param:", empData, roleID);
 
-    var insertSql = "insert into employee set first_name = ?, last_name = ?, role_id = ?"
-    connection.query(insertSql, [empData.firstN, empData.lastN, roleID], function (err, res) {
-        if (err) throw err;
-        console.log("Added Emp: ", res);
-        startMenu();
-    });
+    // If empData.manager !="" Search for Mananger ID.
+    if (empData.manager.length > 0) {
+        var newEmp = await db.addEmp(empData.firstN, empData.lastN, roleID, mgrID);
+    } else {
+        // Else, default mgrID = Null.
+        console.log("No Manager defined");
+        var newEmp = await db.addEmpNoMgr(empData.firstN, empData.lastN, roleID);
+    }
+    //console.log("new employee added", newEmp);
+    startMenu();
 }
 
 
 // removeEmp(): List out all Employee first
 // Then let user choose an employee, and delete
-function removeEmp() {
+async function removeEmp() {
     console.log("Start removeEmp");
 
     // Idea is to construct an array which has all the employee name
@@ -122,21 +166,20 @@ function removeEmp() {
     // Then, use await prompt, list out all choices
     // Once user selected, pass that user to next sql statement to remove.
 
-    /*
-    var allEmpSql = "select last_name, first_name from employee"
-    connection.query(allEmpSql, function (err, res) {
-        if (err) throw err;
-        // console.log("Added Emp: ", res);
-        console.table(res);
-    */
-}
 
+}
 
 async function viewEmpScreen() {
     const empList = await db.listAllEmp();
-    //console.log("viewEmpScreen", empList);
     console.table(empList);
+    startMenu();
 }
 
+
+// Quit() would just not return to the menu
+async function quit() {
+    console.log("Good bye");
+    process.exit();
+}
 
 
--- sqlTask/index.js
+++ sqlTask/index.js
@@ -6,25 +6,78 @@
 const connection = require("./connection");
 
 class empCRUD {
-    constructor (connection) {
+    constructor(connection) {
         this.connection = connection;
     }
 
     // List all employee info 
     listAllEmp() {
-        var listEmp = "select * from employee;"
-
-        // this connection.query return result
-        //this.connection.query(listEmp, function (err, res) {
-        //    console.log("query result", res);
-        //})
-
-        // This, however, does not...
+        // Notice at the end , we rename the table name from employee to manager. If keep using employee.id = employee.manager_id
+        // you will get an error "Not unique table/alias"
+        var listEmp = "select employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.name as department , manager.first_name as manager from employee left join roles on employee.role_id  = roles.id left join department on roles.department_id = department.id left join employee manager on manager.id = employee.manager_id;"
         return this.connection.query(listEmp);
-        //console.log("executed query", emplist);
-        //return emplist;
     }
-}
+
+    // ListAllDept
+    listAllDept() {
+        var listDeptSql = "select name, id from department;"
+        return this.connection.query(listDeptSql);
+
+    }
+
+    // Check if dept exists
+    ifDeptExist(dept) {
+        var deptExist = "select name from department where name = ?;"
+        return this.connection.query(deptExist, [dept]);
+    }
+
+    // Add new department 
+    addDept(dept) {
+        var insertSql = "insert into department set name = ?;"
+        return this.connection.query(insertSql, [dept]);
+    }
+
+    // Retrieve dept ID , so Roles table use the same dept ID 
+    getDeptID(dept) {
+        var deptIDSql = "select id from department where name = ?;"
+        return this.connection.query(deptIDSql, [dept]);
+    }
+
+    // check if current role exists
+    ifRoleExist(role) {
+        var roleExist = "select title from roles where title = ?;"
+        return this.connection.query(roleExist, [role]);
+    }
+
+    // Add new Role
+    addRole(role, salary, deptID) {
+        var insertSql = "insert into roles set title = ?, salary = ?, department_id = ?;"
+        return this.connection.query(insertSql, [role, salary, deptID]);
+    }
+
+    // Retrieve role ID , so emp table use the same role ID
+    getRoleID(role) {
+        var roleIDSql = "select id from roles where title = ?;"
+        return this.connection.query(roleIDSql, [role]);
+    }
+
+
+    // Add New Employee
+    addEmp(fname, lname, roleID, mgrID) {
+        var insertSql = "insert into employee set first_name = ?, last_name = ?, role_id = ?, manager_id=?;"
+        return this.connection.query(insertSql, [fname, lname, roleID, mgrID]);
+    }
+
+    // Add New Employee
+    addEmpNoMgr(fname, lname, roleID) {
+        var insertSql = "insert into employee set first_name = ?, last_name = ?, role_id = ?;"
+        return this.connection.query(insertSql, [fname, lname, roleID]);
+    }
+
+
+
+
+} // end Class
 
 // Here we initiated an instace of class empCURD by passing in the connection obj in
-module.exports = new empCRUD (connection)
+module.exports = new empCRUD(connection)
